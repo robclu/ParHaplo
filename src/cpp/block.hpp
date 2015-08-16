c06ee4d06ca53ec4c43a54ed9623c4bea7a92732
robclu <robclu1818@gmail.com>
2015-08-16 11:40:10 +0200

    Added Block data getters and setters

--- src/cpp/block.hpp
+++ src/cpp/block.hpp
@@ -38,13 +38,54 @@ public:
     /// @brief  Defult constructor, intitlizes the data to empty
     // ------------------------------------------------------------------------------------------------------
     Block() : _data(Rows * Cols) {}
- 
+
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Constructs a Block from a block expression
+    /// @param[in]  expression  The expression to create the Block from
+    /// @tparam     Expression  The type of expression 
+    // ------------------------------------------------------------------------------------------------------
+    template <typename Expression>
+    Block(BlockExpression<T, Expression> const& expression) 
+    {
+        Expression const& expr = expression;
+        _data.resize(expr.size());
+        for (size_type i = 0; i < expr.size(); ++i) {
+            _data[i] = expr[i];                                     // Put data from expr into this block
+        }
+    }
+    
     // ------------------------------------------------------------------------------------------------------
     /// @brief  Gets the size of the block (total number of elements)
     /// @return The total number of elements in the block
     // ------------------------------------------------------------------------------------------------------
-    size_type size() const { return _data.size(); }
+    inline size_type size() const { return _data.size(); }
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Assigns a pointer to data to the vector so that we can use the vector as data rather than 
+    ///             the raw pointer. There shouldn't be any performance loss, but we will have to test that
+    /// @param[in]  data_source     A reference to the data array
+    /// @tparam     N               The number of elements in the data array
+    template <std::size_t N>
+    inline void assign_data(const T (&data_source)[N])
+    {
+        static_assert( N == (Rows * Cols), "Invalid data source size for block" );
+        _data.assign(data_source, data_source + N);
+    }
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Gets the Block's data
+    /// @return     A reference to the Block's data
+    // ------------------------------------------------------------------------------------------------------
+    inline container_type const& get_data() const { return _data; }
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Gets input data for a block from a file
+    /// @param[in]  filename    The file to get the input data from
+    // ------------------------------------------------------------------------------------------------------
 };
+
+// ---------------------------------------- Implementations -------------------------------------------------
+
 
 }           // End namespace phap
 
--- src/parahaplo_cpp_tests.cpp
+++ src/parahaplo_cpp_tests.cpp
@@ -22,5 +22,21 @@ BOOST_AUTO_TEST_CASE( canCreateABlockOfAnyTypeAnSize )
     BOOST_CHECK( block_99.size() == 81 );
 }
 
+BOOST_AUTO_TEST_CASE( canAssignAndGetBlockData )
+{
+    phap::Block<int, 2, 2>      block_22;
+    int                         data[4] = {1, 2, 3, 4};
+    
+    // Assign data - static assert for dimensions mismatch
+    block_22.assign_data(data);
+    
+    // Get a reference to the data
+    const std::vector<int>& block_22_data = block_22.get_data();
+    
+    BOOST_CHECK( block_22_data[0] == 1 );
+    BOOST_CHECK( block_22_data[1] == 2 );
+    BOOST_CHECK( block_22_data[2] == 3 );
+    BOOST_CHECK( block_22_data[3] == 4 );
+}
 
 BOOST_AUTO_TEST_SUITE_END()
