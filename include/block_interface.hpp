5b835d7ff5c3019ade95166b825eee7ce49c6760
robclu <robclu1818@gmail.com>
2015-09-17 12:24:57 +0200

    Finished refactoring block interface in /include

--- include/block_interface.hpp
+++ include/block_interface.hpp
@@ -21,14 +21,26 @@ namespace haplo {
 template <typename Implementation>
 class BlockInterface : Implementation {
 public:
-    // Type alias for the block type 
-    using block_type = Implementation;
-
+    // ------------------------------------------ ALIASES ---------------------------------------------------
+    using block_type        = Implementation;
+    using reference_type    = typename block_type::reference_type;
+    using value_type        = typename block_type::value_type;
+    using read_type         = typename block_type::read_type;
+    using subinfo_type      = typename block_type::subinfo_type;
+    // ------------------------------------------------------------------------------------------------------
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Constructor for the block, this constructor must be used otherwise the behaviour is
+    ///             undefined since the block will not have any data
+    /// @param[in]  data_file   The name of the data file to get the data from
+    // ------------------------------------------------------------------------------------------------------
+    explicit BlockInterface(const char* data_file) : Implementation(data_file) {}
+    
     // ------------------------------------------------------------------------------------------------------
     /// @brief      Gets a pointer to the base class (block implementation)
     /// @return     A pointer to the base class
     // ------------------------------------------------------------------------------------------------------
-    Implementation* implementation() { return static_cast<Implementation*>(*this); }
+    Implementation* implementation() { return static_cast<Implementation*>(this); }
    
     // ------------------------------------------------------------------------------------------------------
     /// @brief      Gets a contant pointer to the base class (implementation)
@@ -42,17 +54,54 @@ public:
     // ------------------------------------------------------------------------------------------------------
     static constexpr size_t num_cores() { return block_type::num_cores(); }
 
+    //-------------------------------------------------------------------------------------------------------
+    /// @brief      Gets the number of rows in the block 
+    /// @return     The number of rows in the block
+    //-------------------------------------------------------------------------------------------------------
+    static constexpr size_t rows() { return block_type::rows(); }
+   
+    //-------------------------------------------------------------------------------------------------------
+    /// @brief      Gets the number of columns in the block
+    /// @return     The number of columns in the block
+    //-------------------------------------------------------------------------------------------------------
+    static constexpr size_t cols() { return block_type::cols(); }     
+
     // ------------------------------------------------------------------------------------------------------
     /// @brief      Gets the size of the block 
     /// @return     The size of the block 
     // ------------------------------------------------------------------------------------------------------
     static constexpr size_t size() { return block_type::size(); }
+
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Gets the information for the sub block i of this block. This will throw an out of range
+    ///             error if the sub-block for which the information is wanted is not valid
+    /// @param[in]  i   The index of the sub-block for which the information must be given
+    /// @return     The subblock information for the sub-block with index i
+    // ------------------------------------------------------------------------------------------------------
+    const subinfo_type& subblock_info(const size_t i) const { return implementation()->subblock_info(i); }
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Gets a refernce to the element of the block at row and column -- does not implement bound
+    ///             checking as this is optimozed for performance, .at() can be implemented at a later stage 
+    ///             if bound checking access is required
+    /// @param[in]  row     The row of the element in the block
+    /// @param[in]  col     The column of the element in the block 
+    // ------------------------------------------------------------------------------------------------------
+    reference_type operator()(size_t row, size_t col) { return implementation()->operator()(row, col); }
+   
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Gets the read at row i of the block -- nor error checking for performance, again .at can
+    ///             be implemented at a later stage
+    /// @param[in]  The index of the read to get from the block
+    /// @return     The read at row i in the block
+    // ------------------------------------------------------------------------------------------------------
+    const read_type& operator[](size_t i) const { return implementation()->operator[](i); }
     
     // ------------------------------------------------------------------------------------------------------
     /// @brief      Fills the block with data
     /// @param[in]  filename    The name of the file which containes the data to fill the block with
     // ------------------------------------------------------------------------------------------------------
-    void fill(const std::string filename) { implementation()->fill(filename); }
+    void fill(const char* filename) { implementation()->fill(filename); }
 };
 
 // ----------------------------------------------------------------------------------------------------------
--- include/parahaplo_tests.cpp
+++ include/parahaplo_tests.cpp
@@ -13,6 +13,10 @@
 
 using device = haplo::Device;
 
+// ---------------------------------------------- INPUT FILES -----------------------------------------------
+
+constexpr char* input_simple = "data/test_input_file.txt";
+
 // ----------------------------------------- BLOCK TESTS -----------------------------------------------------
 
 BOOST_AUTO_TEST_SUITE( BlockTestSuite )
@@ -22,17 +26,55 @@ BOOST_AUTO_TEST_CASE( canCreateCpuBlock )
     haplo::Block<6          ,                   // 6 rows
                  7          ,                   // 7 columns
                  4          ,                   // 4 cores
-                 device::CPU> cpu_block;
+                 device::CPU> cpu_block(input_simple);
     
-    BOOST_CHECK( cpu_block.num_cores() == 4 );
+    BOOST_CHECK( cpu_block.num_cores()  == 4 );
+    BOOST_CHECK( cpu_block.rows()       == 6 );
+    BOOST_CHECK( cpu_block.cols()       == 7 );
 }
 
-BOOST_AUTO_TEST_CASE( canFillCpuBlock )
-{
-    haplo::Block<6, 6, 3, device::CPU> cpu_block;
-    //cpu_block.fill(input_file);
+BOOST_AUTO_TEST_CASE( blockFillsDataWithConstructor )
+{  
+    // Data filled during construction
+    haplo::Block<10, 7, 5, device::CPU> cpu_block(input_simple);
     
-    BOOST_CHECK( cpu_block.size() == 36 );
+    BOOST_CHECK( cpu_block.size() == 70 );
+    BOOST_CHECK( cpu_block(0, 0) == 0 );
+    BOOST_CHECK( cpu_block(0, 1) == 2 );
+    BOOST_CHECK( cpu_block(1, 0) == 1 );
+    BOOST_CHECK( cpu_block(1, 1) == 0 );
+    BOOST_CHECK( cpu_block(2, 2) == 0 );
+    BOOST_CHECK( cpu_block(5, 3) == 0 );
+    BOOST_CHECK( cpu_block(8, 5) == 2 ); 
+}
+
+BOOST_AUTO_TEST_CASE( blockDeterminesReadInfoCorrectly )
+{
+    // Read info determined during construction
+    haplo::Block<10, 7, 6, device::CPU> cpu_block(input_simple);
+    
+    BOOST_CHECK( cpu_block[0].start()    == 0 );
+    BOOST_CHECK( cpu_block[0].end()      == 0 );
+    BOOST_CHECK( cpu_block[0].length()   == 1 );
+    BOOST_CHECK( cpu_block[4].start()    == 2 );
+    BOOST_CHECK( cpu_block[4].end()      == 3 );
+    BOOST_CHECK( cpu_block[4].length()   == 2 ); 
+    BOOST_CHECK( cpu_block[8].start()    == 3 );
+    BOOST_CHECK( cpu_block[8].end()      == 6 );
+    BOOST_CHECK( cpu_block[8].length()   == 4 );
+} 
+
+BOOST_AUTO_TEST_CASE( canGetUnsplittableSubBlockInfo )
+{
+    // Constructor determines subblock info 
+    haplo::Block<10, 7, 6, device::CPU> cpu_block(input_simple);
+
+    BOOST_CHECK( cpu_block.subblock_info(0).start() == 0 );    
+    BOOST_CHECK( cpu_block.subblock_info(0).end()   == 2 );    
+    BOOST_CHECK( cpu_block.subblock_info(1).start() == 2 );    
+    BOOST_CHECK( cpu_block.subblock_info(1).end()   == 3 );    
+    BOOST_CHECK( cpu_block.subblock_info(2).start() == 3 );    
+    BOOST_CHECK( cpu_block.subblock_info(2).end()   == 6 );    
 }
 
 BOOST_AUTO_TEST_SUITE_END()
