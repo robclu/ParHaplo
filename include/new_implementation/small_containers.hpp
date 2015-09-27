71edc5f81d5dcb44c8e8016c378ba03d0c922a3b
robclu <robclu1818@gmail.com>
2015-09-28 01:01:03 +0200

    Added bit removal to small container classes

--- include/new_implementation/small_containers.hpp
+++ include/new_implementation/small_containers.hpp
@@ -18,9 +18,27 @@ using byte = uint8_t;
 
 
 // ----------------------------------------------------------------------------------------------------------
+/// @struct     BinaryOnes
+/// @brief      Generates a seqeunce of binary ones at compile time, the length depends on the data type
+/// @tparam     SizeType    The type used to determine the size
+// ----------------------------------------------------------------------------------------------------------
+template <typename SizeType>
+struct BinaryOnes {
+    static constexpr size_t num_elements = sizeof(SizeType) * 8;
+    static constexpr SizeType value = std::numeric_limits<SizeType>::max() ^ (1 << (num_elements - 1));
+};
+
+// specialization for 8 bit unsigned
+template <>
+struct BinaryOnes<byte> {
+    static constexpr byte value = std::numeric_limits<byte>::max();
+};
+
+// ----------------------------------------------------------------------------------------------------------
 /// @class      TinyContainer
 /// @brief      Contianer for binary variables which uses 1 bit pre varable and can hold up to 64 binary
-///             variables 
+///             variables (DON'T use long or unsigned int -- they aren't working fir bit removal
+///             so use byte (8 bits), short (16 bits), int (32 bits), unsigned lon (64 bits)
 /// @tparam     SizeType        The type used for the size of the container -- byte for 8 bits, short for 16 
 ///             bits, int for 32, long for 64 bits etc...
 /// @tparam     BitsPerElement  The number of bits per element. The default is 1 bit, but 2 can be used
@@ -58,25 +76,30 @@ public:
         // If the value is valid and values != value, then we must swap the values
         if (value <= 1 && ((_bits >> i & 0x01) ^ (value & 0x01))) _bits ^= (0x01 << i);
     }
-    
+
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Removes a bit from the container, and shifts all other bits left, so essentially it's like
+    ///             removing and element from the container.
+    /// @param[in]  i   The index of the bit to remove
+    // ------------------------------------------------------------------------------------------------------
     inline void remove_bit(const byte i) 
     {
-        //SizeType ones = (1 << (sizeof(SizeType) * 8)) - 1;
-        //SizeType oth = ((1 << (sizeof(SizeType) * 8 - 1)) - 1);
-        std::bitset<sizeof(SizeType)*8> a(std::numeric_limits<SizeType>::max() ^ (1 << (sizeof(SizeType) * 8 - 1)));
-        //std::bitset<sizeof(SizeType)*8> a(((1 << (sizeof(SizeType) * 8)) - 1) - ((1 << (i + 1)) - 1));
-        //std::bitset<sizeof(SizeType)*8> a((_bits & ((1 << i) - 1)) << 1);
-        //std::bitset<sizeof(SizeType)*8> a(oth);
-        std::cout << a << "\n" << /*b <<*/ "\n";
-            
-        _bits = (_bits & (0xFF - ((1 << (i + 1)) - 1)))        // Get high bits
-                ^ ( _bits & ((1 << i) - 1) << 1);
+        // All ones -- works for signed and unsigned types
+        static constexpr SizeType ones = BinaryOnes<SizeType>::value;
+        _bits = (_bits & (ones - ((1 << (i + 1)) - 1)))     // Get bits to the left of the bit to remove
+                ^ (( _bits & ((1 << i) - 1)) << 1);         // Get bits below the bits to move
     }
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Shifts all bits left by n 
+    /// @param[in]  n   THe amount to shift the bits left, default to 1
+    // ------------------------------------------------------------------------------------------------------
+    inline void shift_left(const size_t n = 1) { _bits <<= n; }
     
     void print()
     {
         std::bitset<sizeof(SizeType) * 8> x(_bits);
-        std::cout << x << "\n";
+        std::cout << x;
     }
 };
 
@@ -112,11 +135,37 @@ public:
         // Check if the higher (left) bit has the correct value, otherwise set it
         if (value <= 3 && ((_bits >> (i * 2 + 1) & 0x01) ^ (value >> 1 & 0x01))) _bits ^= (0x01 << (i * 2 + 1));
     }
+
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Removes a bit from the container, and shifts all other bits left, so essentially it's like
+    ///             removing and element from the container.
+    /// @param[in]  i   The index of the bit to remove
+    // ------------------------------------------------------------------------------------------------------
+    inline void remove_bit(const byte i) 
+    {
+        // All ones -- works for signed and unsigned types
+        static constexpr SizeType ones = BinaryOnes<SizeType>::value;
+        _bits = (_bits & (ones - ((1 << (2 * i + 2)) - 1)))     // Get bits to the left of the bit to remove
+                ^ (( _bits & ((1 << (2 * i)) - 1)) << 2);        // Get bits below the bits to move
+    }
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Shifts all bits left by n 
+    /// @param[in]  n   THe amount (number of positions) to shift the bits left, default to 1
+    // ------------------------------------------------------------------------------------------------------
+    inline void shift_left(const size_t n = 1) { _bits <<= (2 * n); }
+    
+    void print()
+    {
+        std::bitset<num_elements * 2> x(_bits);
+        std::cout << x;
+    }
 };
 
 // ----------------------------------------------------------------------------------------------------------
 /// @class  BinaryContainer 
-/// @brief  Conatiner which can hold N binary variables, which is optimized for space and performance.
+/// @brief  Container which can hold N binary variables, which is optimized for space and performance. The
+///         bits are stored little endian
 /// @tparam NumElements     The number of binary elements the container can hold
 /// @tparam BitsPerElement  The number of bits per element, can be 1 or 2 -- default to 1
 // ----------------------------------------------------------------------------------------------------------
@@ -130,25 +179,62 @@ public:
     static constexpr size_t bins                = NumElements / elements_per_bin;;
 private:
     internal_container      _data[bins + 1];    //!< Array of bit containers
+    size_t                  _num_elements;      //!< Number of elements in the container
 public:
     // ------------------------------------------------------------------------------------------------------
     /// @brief      Default constructor
     // ------------------------------------------------------------------------------------------------------
-    BinaryContainer() {}
+    BinaryContainer() : _num_elements(NumElements) {}
     
     // ------------------------------------------------------------------------------------------------------
     /// @brief      Gets a value from the binary container
     /// @param[in]  i   The index of the binary in the container to get
     // ------------------------------------------------------------------------------------------------------
-    inline byte get(const size_t i) const { return _data[i / elements_per_bin].get(i % elements_per_bin); }
+    inline byte get(const size_t i) const 
+    { 
+        return _data[bins - (i / elements_per_bin)].get(i % elements_per_bin); }
     
     // ------------------------------------------------------------------------------------------------------
     /// @brief      Sets a value in the variable container
     /// @param[in]  i       The index of the variable to set
     /// @param[in]  value   The value to set the varibale to (must be 0 or 1)
     // ------------------------------------------------------------------------------------------------------
-    inline void set(const size_t i, const byte value) { _data[i / elements_per_bin].set(i % elements_per_bin, 
-                                                                                        value               ); }
+    inline void set(const size_t i, const byte value) 
+    { 
+        _data[bins - (i / elements_per_bin)].set(i % elements_per_bin, value); 
+    }
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      The size (number of elements) in the container 
+    /// @return     The number of elements in the container 
+    // ------------------------------------------------------------------------------------------------------
+    inline size_t size() const { return _num_elements; }
+    
+    // ------------------------------------------------------------------------------------------------------
+    /// @brief      Removes an element from the container
+    /// @param[in]  i   The index of the element to remove
+    // ------------------------------------------------------------------------------------------------------
+    inline void remove_element(const size_t i) 
+    {
+        // Decrase the number of elements
+        --_num_elements;
+        
+        // First remove the bit which is the element
+        _data[bins - (i / elements_per_bin)].remove_bit(i % elements_per_bin);
+
+        // Now for each of the bins to the right, set the LSB of the container to the MSB of the
+        // container to the right, then shift all bits of the container to the right, to the left
+        for (size_t bin = bins - (i / elements_per_bin); bin < bins; ++bin) {
+            _data[bin].set(0, _data[bin + 1].get(elements_per_bin - 1));
+            _data[bin + 1].shift_left(1);
+        }
+    }
+    
+    void print() 
+    {
+        for (int i = 0; i < bins + 1; ++i)
+            _data[i].print();
+    }
 };
 
 
--- include/new_implementation/tests/small_container_tests.cpp
+++ include/new_implementation/tests/small_container_tests.cpp
@@ -32,7 +32,6 @@ BOOST_AUTO_TEST_CASE( canCreateBinaryContainerWith1BitPerElement )
     BOOST_CHECK( elements.get(7) == 1 );
     BOOST_CHECK( elements.get(8) == 0 );
     BOOST_CHECK( elements.get(9) == 1 );
-    BOOST_CHECK( sizeof(elements) == 2);
 }
 
 BOOST_AUTO_TEST_CASE( canCreateBinaryContainerWith2BitsPerElement ) 
@@ -54,24 +53,106 @@ BOOST_AUTO_TEST_CASE( canCreateBinaryContainerWith2BitsPerElement )
     BOOST_CHECK( elements.get(7)  == 1 );
     BOOST_CHECK( elements.get(8)  == 0 );
     BOOST_CHECK( elements.get(14) == 3 );
-    BOOST_CHECK( sizeof(elements) == 5);
 }
 
-BOOST_AUTO_TEST_CASE( canRemoveBitsFromBinaryContainer ) 
+BOOST_AUTO_TEST_CASE( canRemoveBitsFrom1BitBinaryContainer ) 
 {
     // Define the binary container to use 1 bit per element (default setting)
-    haplo::TinyContainer<int, 1> bits;
+    haplo::TinyContainer<haplo::byte, 1> bits;
     
     // Set some elements
     bits.set(1, 1);
     bits.set(2, 1);
     bits.set(7, 1);
     
-    bits.print();
-    
-    bits.remove_bit(3);
-    
-    bits.print();
+    bits.remove_bit(7);
+
+    BOOST_CHECK( bits.get(7) == 0 );
+    BOOST_CHECK( bits.get(1) == 0 );
+    BOOST_CHECK( bits.get(2) == 1 );
+    BOOST_CHECK( bits.get(3) == 1 );
 }
+
+BOOST_AUTO_TEST_CASE( canRemoveBitsFrom2BitBinaryContainer ) 
+{
+    // Define the binary container to use 1 bit per element (default setting)
+    haplo::TinyContainer<haplo::byte, 2> bits;
     
+    // Set some elements
+    bits.set(0, 3);
+    bits.set(1, 1);
+    bits.set(2, 2);
+    
+    bits.remove_bit(2);
+    
+    bits.shift_left(1);
+    
+    BOOST_CHECK( bits.get(0) == 0 );
+    BOOST_CHECK( bits.get(1) == 0 );
+    BOOST_CHECK( bits.get(2) == 3 );
+    BOOST_CHECK( bits.get(3) == 2 );
+}
+
+BOOST_AUTO_TEST_CASE( canRemoveElementsOf1BitContainer ) 
+{
+    // Define the binary container to use 1 bit per element (default setting)
+    haplo::BinaryContainer<12> elements;
+    
+    // Set some elements
+    elements.set(1, 1);
+    elements.set(2, 1);
+    elements.set(6, 1);
+    elements.set(7, 1);
+    elements.set(9, 1);    
+    elements.set(3, 0);
+    
+    elements.remove_element(6);
+    elements.remove_element(7);
+
+    BOOST_CHECK( elements.get(1) == 0 );
+    BOOST_CHECK( elements.get(2) == 0 );
+    BOOST_CHECK( elements.get(3) == 1 );
+    BOOST_CHECK( elements.get(4) == 1 );
+    BOOST_CHECK( elements.get(5) == 0 );
+    BOOST_CHECK( elements.get(6) == 0 );
+    BOOST_CHECK( elements.get(7) == 0 );
+    BOOST_CHECK( elements.get(9) == 1 );
+    BOOST_CHECK( elements.size() == 10 );
+}
+
+BOOST_AUTO_TEST_CASE( canRemoveElementsOf12BitContainer ) 
+{
+    // Define the binary container to use 2 bits per element
+    haplo::BinaryContainer<14, 2> elements;
+    
+    // Set some elements
+    elements.set(1, 1);
+    elements.set(2, 2);
+    elements.set(6, 0);
+    elements.set(7, 3);
+    elements.set(9, 2);    
+    elements.set(3, 0);
+
+    elements.print();
+
+    std::cout << "\n";    
+    elements.remove_element(6);
+    
+    elements.print(); std::cout << "\n";
+    
+    elements.remove_element(2);
+
+    BOOST_CHECK( elements.get(1) == 0 );
+    BOOST_CHECK( elements.get(2) == 0 );
+    BOOST_CHECK( elements.get(3) == 1 );
+    BOOST_CHECK( elements.get(4) == 0 );
+    BOOST_CHECK( elements.get(5) == 0 );
+    BOOST_CHECK( elements.get(6) == 0 );
+    BOOST_CHECK( elements.get(7) == 3 );
+    BOOST_CHECK( elements.get(9) == 2 );
+    BOOST_CHECK( elements.size() == 12 );
+    
+    elements.print();
+}
+
 BOOST_AUTO_TEST_SUITE_END()
