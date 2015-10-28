067faf168cd95f997d907e8d9a8c7f2942b46e2b
robclu <robclu1818@gmail.com>
2015-10-28 16:37:53 +0200

    Edited tests

--- tests/cuda_graph_tests.cu
+++ tests/cuda_graph_tests.cu
@@ -17,13 +17,13 @@
 using namespace std::chrono;
 
 static constexpr const char* test_input    = "output_files/output_simulated_265.txt";
-static constexpr const char* test_input1   = "new_outputs/geraci/100_10_0.1_0.4/output_5298.txt";
+static constexpr const char* test_input1   = "geraci_0.1/100_10_0.1_0.4/output_5473_1.txt";
 
 BOOST_AUTO_TEST_SUITE( GraphGpuSuite )
 
 BOOST_AUTO_TEST_CASE( canCreateGraph )
 {
-    using block_type    = haplo::Block<5298, 4, 4>;
+    using block_type    = haplo::Block<5473, 4, 4>;
     using subblock_type = haplo::SubBlock<block_type, 4, 4, haplo::devices::cpu>;
     using graph_type    = haplo::Graph<subblock_type, haplo::devices::gpu>;
 
